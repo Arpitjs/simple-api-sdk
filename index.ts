0171bf74331a6bb684e3b73b66800dd1312ff4ce
arpitjs <arpited7@gmail.com>
2022-04-19 22:35:09 +0545

    refactor sdk

--- index.ts
+++ index.ts
@@ -5,20 +5,22 @@ export default class SimpleAPISDK {
   url: string;
 
   constructor(server: string, routeName: string) {
-    this.url =server + "/" +routeName;
+    this.url = server + "/" + routeName;
   }
 
-  async getAll<ResponseType = AxiosResponse<any[]>>(): Promise<ResponseType> {
+  async getAll<ResponseType extends AxiosResponse>(): Promise<ResponseType> {
     const response: ResponseType = await axios.get(this.url);
     return response;
   }
 
-  async post(data: Product): Promise<ResponseType> {
+  async post<ResponseType extends AxiosResponse>(
+    data: Product
+  ): Promise<ResponseType> {
     var response: ResponseType = await axios.post(this.url, data);
     return response;
   }
 
-  async patch(
+  async patch<ResponseType extends AxiosResponse>(
     id: number | string,
     data: Product
   ): Promise<ResponseType> {
@@ -26,7 +28,7 @@ export default class SimpleAPISDK {
     return response;
   }
 
-  async delete(
+  async delete<ResponseType extends AxiosResponse>(
     id: number | string
   ): Promise<ResponseType> {
     var response: ResponseType = await axios.delete(this.url + `/${id}`);

3f95052f2c077b4d62ddddfd2f9b770374b22058
arpitjs <arpited7@gmail.com>
2022-04-19 21:53:48 +0545

    added post/patch/delete in sdk

--- index.ts
+++ index.ts
@@ -1,15 +1,35 @@
-import axios, 
-{ AxiosResponse, AxiosRequestConfig } from 'axios'
+import axios, { AxiosResponse } from "axios";
+import { Product } from "./Product";
 
-export default class MySDK {
-    server: string
-    routeName: string
-    constructor(server: string, routeName: string) {
-        this.server = server
-        this.routeName = routeName
-    }
-    async getAll<ResponseType = AxiosResponse<any[]>>(config?: AxiosRequestConfig) : Promise<ResponseType>{
-        const response: ResponseType = await axios.get(this.server + "/" + this.routeName, config)
-        return response
-    }
+export default class SimpleAPISDK {
+  url: string;
+
+  constructor(server: string, routeName: string) {
+    this.url =server + "/" +routeName;
+  }
+
+  async getAll<ResponseType = AxiosResponse<any[]>>(): Promise<ResponseType> {
+    const response: ResponseType = await axios.get(this.url);
+    return response;
+  }
+
+  async post(data: Product): Promise<ResponseType> {
+    var response: ResponseType = await axios.post(this.url, data);
+    return response;
+  }
+
+  async patch(
+    id: number | string,
+    data: Product
+  ): Promise<ResponseType> {
+    var response: ResponseType = await axios.put(this.url + `/${id}`, data);
+    return response;
+  }
+
+  async delete(
+    id: number | string
+  ): Promise<ResponseType> {
+    var response: ResponseType = await axios.delete(this.url + `/${id}`);
+    return response;
+  }
 }
